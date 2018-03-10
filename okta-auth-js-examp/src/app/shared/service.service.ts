44e2d213ccfa547a5562ddbd6b73ab739c47e749
Tapio Mattila <tapsa872@gmail.com>
2018-03-10 18:35:46 +0200

    Added authorization service

---
+++ okta-auth-js-examp/src/app/shared/service.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import * as OktaAuth from "@okta/okta-auth-js";
+
+@Injectable()
+export class ServiceService {
+
+  oktaAuth = new OktaAuth({
+    url: "",
+    clientId: "",
+    issuer: "/oauth2/default",
+    redirectUri: "/callback"
+  });
+
+  constructor(private router: Router) { }
+
+  isAuthenticated() {
+    // Checks if there is a current accessToken in the TokenManager
+    return !this.oktaAuth.tokenManager.get("accessToken");
+  }
+
+  login() {
+    // Launches the login redirect
+    this.oktaAuth.token.getWithRedirect({
+      responseType: ["id_token", "token"],
+      scopes: ["openid", "email", "profile"]
+    });
+  }
+
+  async handleAuthentication() {
+    const tokens = await this.oktaAuth.token.parseFromUrl();
+    tokens.forEach(token => {
+      if(token.idToken) {
+        this.oktaAuth.tokenManager.add("idToken", token);
+      }
+      if(token.accessToken) {
+        this.oktaAuth.tokenManager.add("accessToken", token);
+      }
+    });
+  }
+
+  async logout() {
+    this.oktaAuth.tokenManager.clear();
+    await this.oktaAuth.signOut();
+  }
+
+
+}
